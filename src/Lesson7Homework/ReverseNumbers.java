eabd19cdb1fb327485f7ad6e317e0623f2817133
ifantaziu <roscairina012@gmail.com>
2025-03-17 19:35:10 +0300

    lesson 7 homework optimization

--- src/Lesson7Homework/ReverseNumbers.java
+++ src/Lesson7Homework/ReverseNumbers.java
@@ -1,15 +1,19 @@
 package Lesson7Homework;
+
 import java.util.Scanner;
+
 public class ReverseNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Insert the number = ");
         int number = scanner.nextInt();
         int absoluteValue = Math.abs(number);
-        String line = "" + absoluteValue;
-        for (int i = line.length() - 1; i >= 0; --i) {
-            System.out.print(line.charAt(i));
+        int reverseNumber = 0;
+        while (absoluteValue > 0) {
+            int lastDigit = absoluteValue % 10;
+            absoluteValue = absoluteValue / 10;
+            reverseNumber = reverseNumber * 10 + lastDigit;
         }
-        System.out.println();
+        System.out.println("Reversed number: " + reverseNumber);
     }
 }
