e421161401a72059624c894a9eda9db0b5724f8a
„Irina.fantaziu” <roscairina012@gmail.com>
2025-04-10 20:03:23 +0300

    Chapter 8 Homework adjustments

--- src/Chapter8/ParkingCharge.java
+++ src/Chapter8/ParkingCharge.java
@@ -4,32 +4,49 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ParkingCharge {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] clients = new int[10];
-        int[] hoursYesterday = new int[10];
-        double[] parkingFee = new double[10];
-        double totalParkingFee = 0;
-        LocalDate yesterday = LocalDate.now().minusDays(1);
+    Scanner scanner = new Scanner(System.in);
+    int[] clients = new int[10];
+    int[] hoursYesterday = new int[10];
+    double[] parkingFee = new double[10];
+    double totalParkingFee = 0;
+    LocalDate yesterday = LocalDate.now().minusDays(1);
 
+    public static void main(String[] args) {
+
+        ParkingCharge charge = new ParkingCharge();
+        charge.parkedHoursCount();
+        charge.calculateParkingCharges();
+    }
+
+    public void parkedHoursCount() {
         for (int i = 0; i < clients.length; i++) {
             clients[i] = i + 1;
-            do {
+            {
                 System.out.println("Please enter the number of parking hours for " + yesterday + " for client # " + clients[i] + ": ");
                 hoursYesterday[i] = scanner.nextInt();
                 if (hoursYesterday[i] > 24) {
                     System.out.println("You can not park for more than 24 h! Please enter again a value equal or less than 24 h.");
                 }
-            } while (hoursYesterday[i] > 24);
-            parkingFee[i] = calculateCharges(hoursYesterday[i]);
-            totalParkingFee += parkingFee[i];
-            System.out.println("Parking charge fee for client " + clients[i] + " = " + parkingFee[i] + " USD ");
+            }
+
         }
-        System.out.println("Total parking charge fee for " + yesterday + " : " + totalParkingFee + " USD ");
         scanner.close();
     }
 
-    public static double calculateCharges(int hoursParked) {
+    public void calculateParkingCharges() {
+        for (int i = 0; i < clients.length; i++) {
+            clients[i] = i + 1;
+            if (hoursYesterday[i] < 24) {
+                parkingFee[i] = applyCharges(hoursYesterday[i]);
+                totalParkingFee += parkingFee[i];
+            }
+            System.out.println("Parking charge fee for client " + clients[i] + " = " + parkingFee[i] + " USD ");
+        }
+        System.out.println("Total parking charge fee for " + yesterday + " : " + totalParkingFee + " USD ");
+
+    }
+
+    public static double applyCharges(int hoursParked) {
         double parkingFee;
 
         if ((hoursParked >= 0) && (hoursParked <= 3)) {
@@ -43,6 +60,5 @@ public class ParkingCharge {
 
         return parkingFee;
     }
-
 
 }
